ff0b24c017f10119f733b398ee18a282fc150476
coderTtxi12 <oliver.coding8@gmail.com>
2025-06-05 13:21:42 -0600

    feat: implement Pagefind search engine integration, update Discord links and footer copyright

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ import nextra from 'nextra'
 // Set up Nextra with its configuration
 const withNextra = nextra({
   defaultShowCopyCode: true,
+  search: {
+    codeblocks: true
+  }
 })
 
 // Export the final Next.js config with Nextra included
